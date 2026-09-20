50976a917474896ae67575b465d046b90a9bdf22
sidvla8-wq <sidvla8@gmail.com>
2026-09-20 20:03:48 +0300

    homework_templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/create/', views.product_create, name='product_create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.core.paginator import Paginator
+from .models import Product
+from .forms import ProductForm
 
+
+def home(request):
+    """Главная страница со списком товаров (Задача 2 + пагинация)"""
+    products = Product.objects.all()
+    paginator = Paginator(products, 3)  # по 3 товара на страницу
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'catalog/home.html', {'page_obj': page_obj})
+
+
+def product_detail(request, pk):
+    """Страница с подробной информацией о товаре (Задача 1)"""
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
 
 def home(request):
     """Контроллер для отображения домашней страницы (каталог)."""
@@ -22,3 +40,15 @@ def contacts(request):
         print("--------------------------------")
         success = True
     return render(request, "catalog/contacts.html", {"success": success})
+
+def product_create(request):
+    """Форма добавления нового товара (доп. задание)"""
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+
+    return render(request, 'catalog/product_form.html', {'form': form})
